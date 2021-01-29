de4180698249129b115463555a38eff03e7d2334
Ivan Porto Carrero <ivan@flanders.co.nz>
2021-01-29 08:02:30 -0800

    fix format error

--- src/lib.rs
+++ src/lib.rs
@@ -236,9 +236,9 @@ pub enum RedirectBehavior {
 }
 
 impl Display for RedirectBehavior {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:?}", self)
-    }
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    write!(f, "{:?}", self)
+  }
 }
 
 // // test the code examples in README.md
